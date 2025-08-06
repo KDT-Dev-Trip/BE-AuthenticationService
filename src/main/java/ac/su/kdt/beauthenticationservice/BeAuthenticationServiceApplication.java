451de02abd322c9d316a791b757a585a3ad5ce81
YeeunSeok <yes3641@naver.com>
2025-08-06 09:55:09 +0900

    Initial commit

---
+++ src/main/java/ac/su/kdt/beauthenticationservice/BeAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package ac.su.kdt.beauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeAuthenticationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ac/su/kdt/beauthenticationservice/BeAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.su.kdt.beauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
